295e2f150727b412a15fba19af80715391c2b3bc
mktakuya <takuya.mk96@gmail.com>
2014-05-19 17:29:04 +0000

    Add GPIO controller

--- bloom.py
+++ bloom.py
@@ -3,14 +3,28 @@
 
 from flask import Flask, request, session, g, redirect, url_for, \
              abort, render_template, flash
+import RPi.GPIO as GPIO
+import time
+
+IO_NO = 23
 
 app = Flask(__name__)
 
 DEBUG = True
 SECRET_KEY = 'mktaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' #テキトー
 
+GPIO.cleanup()
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(IO_NO, GPIO.OUT)
+GPIO.output(IO_NO, False)
+
 @app.route('/')
 def index():
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(IO_NO, GPIO.OUT)
+    GPIO.output(IO_NO, False)
     return render_template('index.jinja')
 
 @app.route('/bloom')
@@ -20,8 +34,26 @@ def bloom():
 
 @app.route('/bloomed')
 def bloomed():
+    try:
+        GPIO.output(IO_NO, True)
+    except:
+        pass
+
     return render_template('bloomed.jinja')
 
+@app.route('/stop')
+def stop():
+    GPIO.output(IO_NO, False)
+    GPIO.cleanup()
+    return redirect('/')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    try:
+        GPIO.cleanup()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(IO_NO, GPIO.OUT)
+        GPIO.output(IO_NO, False)
+        app.run(host='0.0.0.0')
+    except:
+        GPIO.cleanup()
 
